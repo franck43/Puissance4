d4d90ed9ae2514eec3c178a19c77529f19b664c8
Thorto <theotimdelan@hotmail.fr>
2015-11-05 17:43:51 +0900

    Update TerrainJeux.java
    Petits constructeurs

--- TerrainJeux.java
+++ TerrainJeux.java
@@ -1,10 +1,20 @@
-public class TerrainJeux{
-  Test héhé
+import java.io.*;
+import java.util.*;
 
-
-
-
-
-
-
+public class TerrainJeux
+{
+    private int longueur
+    private int largeur
+    
+    public TerrainJeux(int longueur, int largeur)
+    {
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+    
+    public Terrainjeux()
+    {
+        this.longueur=6;
+        this.largeur=7;
+    }
 }
